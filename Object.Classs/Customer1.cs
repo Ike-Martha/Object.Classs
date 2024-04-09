e32ed51feef71bcaabcd01a10f6f098870331e29
Ike-Martha <amagwen36@gmail.com>
2024-04-09 11:06:09 +0100

    Creating Class

---
+++ Object.Classs/Customer1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectClasss
+{
+    internal class Customer1
+    {
+        public string CustomerID = null;//Unique identifier for this customer
+
+        /// <summary>
+        /// display the customerID field to the console window
+        /// </summary>
+        public void DisplayID(string v)//public method to return nothing
+        {
+            Console.WriteLine(this.CustomerID);  //the This keyword displays the current instance of the class
+        }
+        /// <summary>
+        /// Display supplied text and the customerID field to the console window
+        /// </summary>
+        /// <param name="Text"> Text to prefix the customer ID</param>
+        public void Display(string Text)
+        {
+            Console.WriteLine(Text + ": " + CustomerID);
+        }
+
+        internal void DisplayID()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Object.Classs/Program.cs
+++ Object.Classs/Program.cs
@@ -1,1 +1,31 @@
-﻿
+﻿using ObjectClasss;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesObjects
+{
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+           Customer1 custA = new Customer1();
+           Customer1 custB = new Customer1();
+
+            custA.CustomerID = "AAA";
+            custB.CustomerID = "BBB";
+
+            Console.WriteLine("custA's customer ID = "  + custA.CustomerID);
+            Console.WriteLine("custB's customer ID = "  + custB.CustomerID);
+
+            custA.DisplayID();
+            custB.DisplayID();
+
+            
+
+        }
+    }
+}
